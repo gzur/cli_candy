db4ec4a4f797338731e71932a2afd0e4f720a25a
gzur <gzur@gzur.org>
2014-04-11 11:19:48 +0200

    added better bash history and renamed shell script to reflect that

--- better_bash.sh
+++ better_bash.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+echo Enabling better bash history as per http://blog.sanctum.geek.nz/better-bash-history/
+cat better_bash_hist_bashrc >> ~/.bashrc
 echo Enabling more awesome history search
 cat history-search-inputrc >> ~/.inputrc
+
 echo "bind -f ~/.inputrc" >> ~/.bashrc
 echo 'History search should be enabled for future sessions.
 

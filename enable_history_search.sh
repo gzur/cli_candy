4dd8d0c0b94153f3b989062e5315eec5bd3d5c5b
gzur <gzur@gzur.org>
2014-04-11 10:55:13 +0200

    minor .sh tweaks

--- enable_history_search.sh
+++ enable_history_search.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo Enabling more awesome history search
 cat history-search-inputrc >> ~/.inputrc
-bind -f ~/.inputrc
+echo "bind -f ~/.inputrc" >> ~/.bashrc
+echo 'History search should be enabled for future sessions.
+
+To enable it for the current session execture "bind -f ~/.inputrc\"'
